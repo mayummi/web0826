d818e10777b8300be7f8b4214662fe5d9b29907f
mayummi <8838730@naver.com>
2024-08-27 09:17:37 +0900

    0827

--- cdn/src/01_시작하기.jsx
+++ cdn/src/01_시작하기.jsx
@@ -16,51 +16,45 @@ function Hello() {
     return (
         // JSX의 태그 끝에는 ; 쓰지 않는다.
         // return 안의 root 태그는 1개 이어야 한다.
-        <div style={{ margin: "12px" }}>
+        <div className="card_list">
             {/* XML에서 빈 요소는 끝에 <img /> */}
-            <img
-                src="https://img.megabox.co.kr/SharedImg/2024/07/03/QaslTt607PkNH8mPWm6ZUH3UwDx2bHoH_420.jpg"
-                alt="사랑의 하츄핑"
-            />
+            <div className="card_img">
+                <img
+                    src="https://img.megabox.co.kr/SharedImg/2024/07/03/QaslTt607PkNH8mPWm6ZUH3UwDx2bHoH_420.jpg"
+                    alt="사랑의 하츄핑"
+                />
+            </div>
             <div>
                 {/* {{}} -> 바깥쪽 괄호는 표현식 */}
                 {/* {{}} -> 안쪽 객체 괄호 */}
                 {/* 객체로 스타일을 지정한다. */}
                 {/* 속성명은 카멜표기법으로 쓴다. 예) borderRadius 또는 "border-radius" */}
-                <span
-                    style={{
-                        padding: "0 4px",
-                        borderRadius: "3px",
-                        background: "red",
-                    }}
-                >
-                    All
-                </span>
+                <span className="card_age">All</span>
                 {/* class -> className 으로 기록한다.*/}
                 <h3 className="card_tit">사랑의 하츄핑</h3>
             </div>
             <div>
-                <span>예매율 1%</span>
-                <span>개봉일 2024.08.07</span>
+                <span className="card_rate">예매율 1%</span>
+                <span className="card_date">개봉일 2024.08.07</span>
             </div>
-            <div>
-                <button>❤ 487</button>
-                <button>예매</button>
+            <div className="card_btn">
+                <button className="card_heart">❤ 487</button>
+                <button className="card_reservation">예매</button>
             </div>
         </div>
     );
 }
 
-function App() {
-    return (
-        <div style={{ display: "flex" }}>
-            <Hello />
-            <Hello />
-            <Hello />
-            <Hello />
-        </div>
-    );
-}
+// function App(){
+//   return (
+//     <div style={{display: "flex"}}>
+//       <Hello />
+//       <Hello />
+//       <Hello />
+//       <Hello />
+//     </div>
+//   );
+// }
 
 /* 
     ReactDOM.render(요소, 컨테이너); 
@@ -72,4 +66,14 @@ function App() {
 */
 
 // 출력 -> 렌더링
-ReactDOM.render(<App />, document.querySelector("#root"));
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+ReactDOM.render(
+    <div style={{ display: "flex" }}>
+        <Hello />
+        <Hello />
+        <Hello />
+        <Hello />
+    </div>,
+    document.querySelector("#root")
+);
